ac64b912072ac336979bb389b6e2946b8ce7d567
asmasw6 <asmasalehw@gmail.com>
2025-10-25 22:25:42 +0300

    add env 2

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
 import connectDB from "./configs/db.js";
-import "dotenv/config";
 import { userRouter } from "./routes/userRoute.js";
 import { sellerRouter } from "./routes/sellerRoute.js";
 import { connectCloudinary } from "./configs/cloudinary.js";
@@ -37,8 +36,8 @@ app.use("/api/cart", cartRouter);
 app.use("/api/address", addressRouter);
 app.use("/api/order", orderRouter);
 
-
+/*
 app.listen(port, () => {
   console.log(`server is running on port : ${port}`);
 });
-
+*/
